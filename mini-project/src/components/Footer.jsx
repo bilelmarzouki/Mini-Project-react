19db41cba3ad8400e862a12e7a9a6eea8b34c26b
eren-brcn <sylar144814@gmail.com>
2026-02-09 19:16:11 +0300

    footer

--- mini-project/src/components/Footer.jsx
+++ mini-project/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer-container">
+      <p>© 2026 Made by Bilel and Eren.</p>
+    </footer>
   )
 }
 
--- mini-project/src/pages/About.jsx
+++ mini-project/src/pages/About.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-        <h1>This a project about some json food recipies</h1>
+    <div className="about-page">
+      <h1>This is a project about some json food recipes</h1>
+      
+      <p>Check out our GitHub profiles:</p>
+      
+      <div className="github-links">
+        <a href="https://github.com/bilelmarzouki" target="_blank">My GitHub (Bilel)</a>
+        <br />
+        <a href="https://github.com/eren-brcn" target="_blank">Partner's GitHub(Eren)</a>
+      </div>
     </div>
   )
 }
